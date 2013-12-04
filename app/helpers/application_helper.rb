f93fca130fae70f51abfc8289d4379f405469461
sonali <sonalikatara@gmail.com>
2013-12-04 14:31:45 -0800

    added some functions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+    #returns full title on per-page basis.
+    def full_title(page_title)
+       base_title ="Ruby in Rails App Using Rspec Testing  "
+       if page_title.empty?
+            base_title
+       else
+            '#{base_title} | #{page_title}'
+       end
+    end
+ 
+   #string shuffle function
+    def string_shuffle(s)
+       s.split('').shuffle.join
+    end
 end

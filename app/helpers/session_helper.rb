043e2bf29fb3362b98016b100b46771ab7fcaf73
sonali <sonalikatara@gmail.com>
2013-12-31 23:57:01 -0800

    edited user

--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -7,11 +7,11 @@ module SessionHelper
     self.current_user = user
   end
 
-def signed_in?
+  def signed_in?
     !current_user.nil?
   end
   
-   def sign_out
+  def sign_out
     current_user.update_attribute(:remember_token,
                                   User.encrypt(User.new_remember_token))
     cookies.delete(:remember_token)
@@ -25,5 +25,19 @@ def signed_in?
   def current_user
     remember_token = User.encrypt(cookies[:remember_token])
     @current_user ||= User.find_by(remember_token: remember_token)
+  end
+
+  def current_user?(user)
+     user == current_user
+  end
+
+  def redirect_back_or(default)
+      redirect_to(session[:return_to] || default)
+      session.delete(:return_to)
+  end
+
+
+  def store_location
+     session[:return_to] = request.url if request.get?
   end
 end

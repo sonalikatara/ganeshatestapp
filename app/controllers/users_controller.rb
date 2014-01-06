22bbc53355bb3e6108414021105a212dcbf1101c
sonali <sonalikatara@gmail.com>
2014-01-05 23:52:42 -0800

    finished user destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
- before_action :signed_in_user, only: [:index, :edit, :update]
+ before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
  before_action :correct_user,   only: [:edit, :update]
+ before_action :admin_user, only: :destroy
 
   def new
      @user = User.new
@@ -35,6 +36,8 @@ class UsersController < ApplicationController
         render 'edit'
      end
    end
+
+
   def index
      # @users = User.all  // displays all users at a time 
     
@@ -43,6 +46,13 @@ class UsersController < ApplicationController
      @users = User.paginate(page: params[:page])
 
   end
+
+   def destroy
+     User.find(params[:id]).destroy
+     flash[:sucess] = "User deleted."
+     redirect_to users_url
+   end
+
  private
 
     def user_params
@@ -63,5 +73,9 @@ class UsersController < ApplicationController
      @user = User.find(params[:id])
      redirect_to(root_url) unless current_user?(@user)
    end
+
+   def admin_user
+      redirect_to(root_url) unless current_user.admin?
+   end 
 
 end

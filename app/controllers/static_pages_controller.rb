7d3194637deda9d5b90681bf21836125dd73559d
sonali <sonalikatara@gmail.com>
2014-01-15 12:52:15 -0800

    Add user microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,11 @@
 class StaticPagesController < ApplicationController
   def home
    # when visiting static_pages/home.html.erb rails first executes the code in this action and then goes to generate the view corresponding to this action
-
-  end
+    if signed_in?
+     @micropost = current_user.microposts.build
+     @feed_items = current_user.feed.paginate(page: params[:page])
+    end
+   end
 
   def help
   end
